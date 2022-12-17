09003e28bc64534138a32ca6a23daf882b785f34
morsa <voxfoot@gmail.com>
2022-12-18 00:39:08 +0500

    implement liked

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,22 +2,27 @@ import React from "react";
 import { ReactComponent as LikeComponent } from '../icons/like.svg';
 import { useDispatch } from 'react-redux';
 import { toggleLike } from "../redux/slices/productsSlice";
+import cn from 'classnames';
 
-const Product = ({ product }) => {
+const Product = ({ product: { like, ...rest } }) => {
 	const dispatch = useDispatch();
 
+	const iconClasses = cn("icon", { liked: like });
+	const productClasses = cn("product", { "liked-product": like });
+
 	return (
-		<div className="product">
+		<div className={productClasses}>
 			<div className="product__header">
-				<img className="product__image" src={product.images[0]} alt={product.title} />
+				<img className="product__image" src={rest.images[0]} alt={rest.title} />
 			</div>
 			<div className="product__info">
-				<div className="title p-5">{product.title}</div>
-				<div className="description p-5">{product.description}</div>
-				<div className="footer grid">
-					<div className="price p-5">{`Current price: ${product.price} $`}</div>
-					<LikeComponent onClick={() => dispatch(toggleLike(product.id))} className="like"/>
-				</div>
+				<div className="title p-5">{rest.title}</div>
+				<div className="description p-5">{rest.description}</div>
+				<div className="price p-5">{`Current price: ${rest.price} $`}</div>
+				<LikeComponent 
+					onClick={() => dispatch(toggleLike(rest.id))} 
+					className={iconClasses}
+				/>
 			</div>
 		</div>
 	)
